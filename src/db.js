98d73c5ab1c5e6a321ad6453a09b595d4e81d3d3
Luis Arteaga <arteagaluis123@gmail.com>
2024-08-14 01:00:22 -0500

    ajuste bd

--- src/db.js
+++ src/db.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost/bdnueva');
-    // await mongoose.connect('mongodb://mongo:27017/mydatabase');
+    // await mongoose.connect('mongodb://localhost/bdnueva');
+    await mongoose.connect('mongodb://mongo:27017/mydatabase');
 
     console.log(' >>>DB connected');
   } catch (error) {
