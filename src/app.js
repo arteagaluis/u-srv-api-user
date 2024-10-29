066920338c4541c7dfe3df9d5b53cee034e0d525
Luis Arteaga <arteagaluis123@gmail.com>
2024-10-28 20:23:03 -0500

    Ajuste cors prod

--- src/app.js
+++ src/app.js
@@ -6,10 +6,19 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+// app.use(
+//   cors({
+//     origin: 'http://iot-test.online:3000',
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://iot-test.online:3000',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
 
