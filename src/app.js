1c8f687b59cd4288031f996fa980a3f91480f06e
Luis Enrique Arteaga Ruiz <larteagr@emeal.nttdata.com>
2025-10-06 14:35:27 -0500

    feat: Change origin CORS

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const emailUri =
 
 app.use(
   cors({
-    origin: emailUri,
+    origin: 'https://api.teliot.site/',//emailUri,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
