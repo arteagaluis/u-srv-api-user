f6d56735e6c9e420e2f4d7143bc173ffe99ef29f
arteagaluis <arteagaluis123@gmail.com>
2025-10-06 21:03:02 -0500

    feat: Change origin CORS 2

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ const emailUri =
 
 app.use(
   cors({
-    origin: 'https://api.teliot.site/',//emailUri,
+    origin: 'https://dashboard.teliot.site/',//emailUri,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
